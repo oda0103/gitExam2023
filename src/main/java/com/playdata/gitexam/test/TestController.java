22570c6ac1033aeb215eeee5c04e1ae63efce06c
dahye <oda0103@naver.com>
2023-10-23 11:24:02 +0900

    test

---
+++ src/main/java/com/playdata/gitexam/test/TestController.java
@@ -0,0 +1,5 @@
+package com.playdata.gitexam.test;
+
+public class TestController {
+
+}
---
+++ src/test/java/com/playdata/gitexam/GitExam2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.gitexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExam2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
